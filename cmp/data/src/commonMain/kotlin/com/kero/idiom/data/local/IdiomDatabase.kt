f1b173d1738ad98f1e6a9b9ebd833b950a7b5056
Moon-Metrex <ywlee861009@gmail.com>
2026-03-11 11:47:11 +0900

    Update IdiomDatabase.kt

--- cmp/data/src/commonMain/kotlin/com/kero/idiom/data/local/IdiomDatabase.kt
+++ cmp/data/src/commonMain/kotlin/com/kero/idiom/data/local/IdiomDatabase.kt
@@ -9,7 +9,7 @@ import com.kero.idiom.data.local.model.UserStatsEntity
 
 @Database(
     entities = [IdiomEntity::class, UserStatsEntity::class],
-    version = 3, // UserStatsEntity 추가로 인한 버전 상향
+    version = 4, // isNotificationEnabled 필드 추가로 인한 버전 상향
     exportSchema = false
 )
 abstract class IdiomDatabase : RoomDatabase() {
