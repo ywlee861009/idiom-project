6978130fdadce8bb5ae0e9f99cdd3a0c18527445
Moon <moon@metrex.im>
2026-03-12 14:14:42 +0900

    app upload

--- cmp/composeApp/build.gradle.kts
+++ cmp/composeApp/build.gradle.kts
@@ -64,7 +64,7 @@ android {
         applicationId = "com.kero.idiom"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
     }
 
