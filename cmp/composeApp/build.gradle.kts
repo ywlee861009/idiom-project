6a7ada659d9c0078628823e1e54bafd15d163638
Moon-Metrex <ywlee861009@gmail.com>
2026-03-14 20:41:01 +0900

    앱업뎃

--- cmp/composeApp/build.gradle.kts
+++ cmp/composeApp/build.gradle.kts
@@ -64,8 +64,8 @@ android {
         applicationId = "com.kero.idiom"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.4"
+        versionCode = 9
+        versionName = "1.0.5"
     }
 
     buildFeatures {
