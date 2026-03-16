4a3195d304bf68972a31448a2350dd363fafc056
Moon <moon@metrex.im>
2026-03-16 10:21:47 +0900

    앱 업데이트

--- cmp/composeApp/build.gradle.kts
+++ cmp/composeApp/build.gradle.kts
@@ -64,8 +64,8 @@ android {
         applicationId = "com.kero.idiom"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.5"
+        versionCode = 10
+        versionName = "1.0.6"
     }
 
     buildFeatures {
