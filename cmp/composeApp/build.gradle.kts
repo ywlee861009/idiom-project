8f8cfcadd524f0bb7f8123661365fbf566221a3a
Moon <moon@metrex.im>
2026-03-20 17:19:43 +0900

    build upgrade

--- cmp/composeApp/build.gradle.kts
+++ cmp/composeApp/build.gradle.kts
@@ -64,8 +64,8 @@ android {
         applicationId = "com.kero.idiom"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.0.7"
+        versionCode = 12
+        versionName = "1.0.8"
     }
 
     buildFeatures {
